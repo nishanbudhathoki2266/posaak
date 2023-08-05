588eede44b07c221ced643fa8cfc8823ac69ba5a
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-05 12:36:37 +0545

    Implemented field limiting

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -27,7 +27,23 @@ exports.getAllProducts = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Product.find(JSON.parse(queryStr));
+    let query = Product.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // Limits
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
 
     // Execute the query
     const products = await query;
