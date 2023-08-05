d1bfe16f9e985aeed5bc86dfbcafea17275d69e5
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-05 11:02:16 +0545

    Connected to database with mongoose

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD.replace("@", "%40")
+);
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log("DB Connection Successful!");
+  })
+  .catch((err) => console.log("Error connecting to the database"));
 
 const port = process.env.PORT || 8080;
 
