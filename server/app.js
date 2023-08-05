24afda9f8ddaf0ccec07bd6533c5f00d7f48d314
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-05 14:51:40 +0545

    Added a global error handler

--- server/app.js
+++ server/app.js
@@ -9,9 +9,23 @@ app.use(express.static(`${__dirname}/public`));
 app.use("/api/v1/products", productRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server!`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  // Calling next with error skips all the middlewares and goes to error handling middleware
+  next(err);
+});
+
+// A centralized error handler
+app.use((err, req, res, next) => {
+  // Internal server error 500 code
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
