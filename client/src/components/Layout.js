1d4f2fdd17562777f0695ec26e4ede34e6bf7224
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-05 07:47:13 +0545

    Added an app layout

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Layout({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <h1>Navbar</h1>
+      {children}
+      <h1>Footer</h1>
+    </div>
+  );
 }
 
 export default Layout;
--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import "@/components/Layout";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
