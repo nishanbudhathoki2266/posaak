818c3a3958ff28074cc109a600e8522f38f96ba2
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-05 09:14:58 +0545

    Added some react icons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
 
 function Header() {
   return (
     <header className="text-gray-600 body-font divide-y-4">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col sm:flex-row items-center">
         <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
           <Link
             href="/"
@@ -32,9 +33,15 @@ function Header() {
           />
         </Link>
         <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-          </button>
+          <div className="flex flex-row gap-4">
+            <span className="relative">
+              <AiOutlineShoppingCart className="cursor-pointer text-2xl" />
+              <p className="text-md bg-orange-600 text-white rounded-full block text-center h-6 w-6 absolute top-[-21px] right-[-12px]">
+                3
+              </p>
+            </span>
+            <AiOutlineUser className="cursor-pointer text-2xl" />
+          </div>
         </div>
       </div>
     </header>
