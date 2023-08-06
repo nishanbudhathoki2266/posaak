7d8f769c049ddd92f083c9e55a0da25826f3a30f
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-06 14:50:57 +0545

    Prevented parameter pollution

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -28,6 +29,12 @@ app.use(express.json({ limit: "10kb" }));
 app.use(mongoSanitize());
 
 app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: ["price"],
+  })
+);
 
 app.use(express.static(`${__dirname}/public`));
 
