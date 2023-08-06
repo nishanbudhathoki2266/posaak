9d07a5c25459abb69ce63856024a32d3283ff6f7
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-06 09:44:36 +0545

    Implemented password hashing in document pre save middleware

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Please confirm your password!"],
+    validate: {
+      // This only works on save!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Both the passwords must match!",
+    },
   },
+});
+
+// Encryption for password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // As we don't want to persist passwordConfirm in the database
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
