8594c13b57215bee926b8d0a1fd20a2248058798
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-06 14:48:54 +0545

    Implemented data sanitization

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -22,6 +24,11 @@ const limiter = rateLimit({
 app.use("/api", limiter);
 
 app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/products", productRouter);
