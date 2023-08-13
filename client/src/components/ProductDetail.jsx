84a39617e03d678076d7175d994f165ad6c15ae0
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-13 20:44:34 +0545

    Added a dynamic dropdown

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -59,11 +59,25 @@ function ProductDetail({ product }) {
                 <span className="mr-3">Size</span>
                 <div className="relative">
                   <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                    <option>SM</option>
-                    <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
+                    {product.sizes.map((size) => (
+                      <option key={size} value={size.toLowerCase()}>
+                        {size}
+                      </option>
+                    ))}
                   </select>
+                  <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-4 h-4"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M6 9l6 6 6-6"></path>
+                    </svg>
+                  </span>
                 </div>
               </div>
             </div>
