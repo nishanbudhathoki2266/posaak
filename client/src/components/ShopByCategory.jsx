cbfc67beb2e494e5af8c5f64f4ec6e78409dbd47
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-14 10:08:47 +0545

    Added login and register UI

--- client/src/components/ShopByCategory.jsx
+++ client/src/components/ShopByCategory.jsx
@@ -9,8 +9,8 @@ function ShopByCategory({ categories }) {
           {categories.map((category) => (
             <div key={category._id} className="p-4 md:w-1/4  sm:w-1/2 w-full">
               <div className="border-2 flex items-center justify-center hover:scale-105 transition-all duration-150 bg-emerald-700 border-gray-200  px-4 h-52 w-auto py-6 rounded-lg">
-                <h2 className="title-font font-medium text-3xl text-white">
-                  {category.name}
+                <h2 className="title-font tracking-wide font-medium text-2xl text-white">
+                  {category.name.toUpperCase()}
                 </h2>
               </div>
             </div>
---
+++ client/src/pages/auth/login.js
@@ -0,0 +1,52 @@
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
+import Link from "next/link";
+
+function LoginPage() {
+  return (
+    <section className="text-gray-600 h-[70vh] flex items-center justify-center p-5">
+      <div className="bg-white flex flex-col mx-auto md:w-3/4 lg:w-1/2 w-full p-8">
+        <Heading>Sign in</Heading>
+        <p className="leading-relaxed mb-5 text-gray-600">
+          Access your personal shopping experience 🛒
+        </p>
+        <div className="relative mb-4">
+          <label for="name" className="leading-7 text-sm text-gray-600">
+            Email
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full bg-white rounded border border-gray-300 focus:border-[#67595E] focus:ring-2 focus:ring-[#67595E] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <div className="relative mb-4">
+          <label for="password" className="leading-7 text-sm text-gray-600">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="w-full bg-white rounded border border-gray-300 focus:border-[#67595E] focus:ring-2 focus:ring-[#67595E]  text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <Button
+          variant="primary"
+          className="flex justify-center text-lg uppercase tracking-wide"
+        >
+          Login
+        </Button>
+        <p className="text-md text-gray-500 mt-3">
+          Don't have an account ?{" "}
+          <Link href="/auth/register" className="text-[#67595E] underline">
+            Register
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default LoginPage;
