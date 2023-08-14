3faccfc99eca9885d5d2e171fae46bdf868b26c4
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-14 10:45:49 +0545

    Fixed a little of padding

--- client/src/pages/products/index.js
+++ client/src/pages/products/index.js
@@ -7,7 +7,7 @@ function AllProducts({ products }) {
   return (
     <section className="text-gray-600 px-5 py-8 body-font">
       <Heading position="center">All Products</Heading>
-      <div className="container px-5 py-14 mx-auto">
+      <div className="container px-5 py-10 mx-auto">
         <div className="flex justify-center flex-wrap -m-4">
           {products.map((product) => (
             <ProductCard key={product._id} product={product} />
