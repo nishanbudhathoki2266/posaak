f55e9f2da5cb7217efb065c2ab2d5adec327d5c8
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-14 12:06:25 +0545

    Fixed the padding of hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,7 +5,7 @@ import Heading from "./Heading";
 function Hero() {
   return (
     <section className="text-gray-600">
-      <div className="container mx-auto flex px-5 py-14 md:flex-row md:py-24 flex-col items-center">
+      <div className="container mx-auto flex px-8 py-14 md:flex-row md:py-24 flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <Heading>Elevate Your Style with Posaak</Heading>
           <p className="mb-8 leading-relaxed">
