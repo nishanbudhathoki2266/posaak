57eda60b3d22fd767c817932667dc7e054cf6ffe
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-19 11:17:00 +0545

    Fixed the protected route component a lil bit

--- client/src/components/ProtectedPage.jsx
+++ client/src/components/ProtectedPage.jsx
@@ -1,12 +1,22 @@
+import { getIsLoggedIn } from "@/redux/reducerSlices/userSlice";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { toast } from "react-hot-toast";
+import { useSelector } from "react-redux";
 
-function ProtectedPage({ isLoggedIn, children }) {
+function ProtectedPage({ url, children }) {
   const router = useRouter();
-  if (!isLoggedIn) {
-    router.push("/auth/login");
-    return;
-  }
+  const isLoggedIn = useSelector(getIsLoggedIn);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push(`/${url}`);
+    } else {
+      toast.error("Login required!");
+      router.push("/auth/login");
+    }
+  }, []);
+
   return children;
 }
 export default ProtectedPage;
--- client/src/pages/cart/index.js
+++ client/src/pages/cart/index.js
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 function CartPage() {
   const isLoggedIn = useSelector(getIsLoggedIn);
   return (
-    <ProtectedPage isLoggedIn={isLoggedIn}>
+    <ProtectedPage url="/cart">
       <Heading position="center">The Cart Page</Heading>
     </ProtectedPage>
   );
