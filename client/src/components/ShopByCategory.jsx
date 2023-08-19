61b083d54dfbb2105f6333ae2140d87fe5552046
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-19 09:42:59 +0545

    Customized the category section little

--- client/src/components/ShopByCategory.jsx
+++ client/src/components/ShopByCategory.jsx
@@ -1,22 +1,11 @@
-import Heading from "./Heading";
+import CategoryCard from "./CategoryCard";
 
 function ShopByCategory({ categories }) {
   return (
-    <section className="text-gray-600 px-5 py-8 body-font">
-      <Heading position="center">Shop By Category</Heading>
-      <div className="container px-5 py-14 mx-auto">
-        <div className="flex flex-wrap justify-center -m-4 text-center cursor-pointer">
-          {categories.map((category) => (
-            <div key={category._id} className="p-4 md:w-1/4  sm:w-1/2 w-full">
-              <div className="border-2 flex items-center justify-center hover:scale-105 transition-all duration-150 bg-emerald-700 border-gray-200  px-4 h-52 w-auto py-6 rounded-lg">
-                <h2 className="title-font tracking-wide font-medium text-2xl text-white">
-                  {category.name.toUpperCase()}
-                </h2>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+    <section className="text-gray-600 px-20 py-8 md:py-24  body-font flex justify-center flex-wrap gap-9">
+      {categories.map((category) => (
+        <CategoryCard key={category._id} category={category} />
+      ))}
     </section>
   );
 }
