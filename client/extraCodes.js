336f2ee5cce65aebade0627848ba0776265979ec
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-01 21:09:33 +0545

    Added a logic to get a sum of all numbers in an array

--- client/extraCodes.js
+++ client/extraCodes.js
@@ -58,3 +58,13 @@ variableD["foo"] = "bar";
 // state = { ...state };
 
 // console.log(state);
+
+const sum = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
+
+// console.log(sum([1, 2, 3, 4, 5, 5, 6]));
