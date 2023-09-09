3ee62127faa47a439fb42bbf5193148cf2d85082
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-09 23:37:53 +0545

    Removed virtual properties from the category model

--- server/models/categoryModel.js
+++ server/models/categoryModel.js
@@ -1,23 +1,14 @@
 const mongoose = require("mongoose");
 
-const categorySchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "A category must have its name!"],
-    },
-    description: {
-      type: String,
-      required: [true, "A category must have its description!"],
-    },
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A category must have its name!"],
   },
-  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
-);
-
-categorySchema.virtual("products", {
-  ref: "Product",
-  localField: "products",
-  foreignField: "_id",
+  description: {
+    type: String,
+    required: [true, "A category must have its description!"],
+  },
 });
 
 const Category = mongoose.model("Category", categorySchema);
