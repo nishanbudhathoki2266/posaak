8bbcd839f8f494968369c80db7777069262b18c8
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-12 00:51:14 +0545

    Implemented searching of products by name

--- client/src/pages/products/index.js
+++ client/src/pages/products/index.js
@@ -5,10 +5,12 @@ import Loader from "@/components/Loader";
 import ProductCard from "@/components/ProductCard";
 import { getAllProducts } from "@/utils/api";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { MdOutlineClear } from "react-icons/md";
 
 function AllProducts(props) {
   const router = useRouter();
+  const [query, setQuery] = useState("");
 
   // Error handling
   if (props.error) return <Error error={props.error} />;
@@ -23,6 +25,10 @@ function AllProducts(props) {
       </div>
     );
 
+  const searchedProducts = products?.filter((product) =>
+    product.name.toLowerCase().includes(query.toLocaleLowerCase())
+  );
+
   return (
     <section className="text-gray-600 px-5 py-8 body-font">
       <Heading position="center">All Products</Heading>
@@ -34,13 +40,20 @@ function AllProducts(props) {
           <FilterButton onClick={() => router.push("?sort=name")}>
             Sort by name
           </FilterButton>
+          <input
+            type="text"
+            placeholder="Type to search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            className="border-2 outline-none px-2 py-[5px] rounded-lg"
+          />
           <MdOutlineClear
             className="text-4xl text-black block cursor-pointer hover:-translate-y-[1px] transition-transform ease-out duration-150"
             onClick={() => router.push("/products")}
           />
         </div>
         <div className="flex flex-wrap -m-4">
-          {products.map((product) => (
+          {searchedProducts.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
         </div>
