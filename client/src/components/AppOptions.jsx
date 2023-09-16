412674b55a2324043fe16d762c1ba1a431153f81
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-16 20:23:01 +0545

    Completed small touches on the landing page

--- client/src/components/AppOptions.jsx
+++ client/src/components/AppOptions.jsx
@@ -4,7 +4,7 @@ import { BsApple, BsGooglePlay } from "react-icons/bs";
 function AppOptions() {
   return (
     <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto flex items-center md:flex-row flex-col">
+      <div className="container px-5 pt-24 mx-auto flex items-center md:flex-row flex-col">
         <div className="flex flex-col md:pr-10 md:mb-0 mb-6 pr-0 w-full md:w-auto md:text-left text-center">
           <h2 className="text-sm font-semibold text-[#67595E] tracking-widest mb-1">
             Posaak! now on portables
