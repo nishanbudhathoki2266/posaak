f04f57c08e41b355d62be0174b3a5221a2d85999
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-16 20:29:50 +0545

    Fixed the hero section stylings

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Heading from "./Heading";
 
 function Hero() {
   return (
-    <section className="text-gray-600">
+    <section className="text-gray-600 flex justify-center items-center">
       <div className="container mx-auto flex md:flex-row md:py-24 flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <Heading>Elevate Your Style with Posaak</Heading>
