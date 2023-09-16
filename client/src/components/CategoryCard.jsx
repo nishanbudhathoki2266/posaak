654f3820a4733a64ebbe5077e2ea764b916cde13
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-16 20:23:35 +0545

    Changed the transition style of category card

--- client/src/components/CategoryCard.jsx
+++ client/src/components/CategoryCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function CategoryCard({ category: { name, description }, onClick }) {
   return (
     <div
-      className="flex-1 sm:w-[350px] bg-slate-50 sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 shadow-lg cursor-pointer hover:-translate-y-2 transition-transform duration-200"
+      className="flex-1 sm:w-[350px] bg-slate-50 sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 shadow-lg cursor-pointer hover:-translate-y-2 hover:scale-105 transition-transform duration-200"
       onClick={onClick}
     >
       <h3 className="mt-5  text-3xl leading-normal font-bold">{name}</h3>
