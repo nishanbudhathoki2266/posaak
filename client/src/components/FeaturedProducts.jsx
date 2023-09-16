01ff9d482615051266658ffbb706f041f711e3f7
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-16 20:26:50 +0545

    Completed the landing page

--- client/src/components/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts.jsx
@@ -5,7 +5,7 @@ function FeaturedProducts({ featuredProducts }) {
   return (
     <section className="text-gray-600 py-14 md:py-24 body-font">
       <Heading position="center">Featured Products</Heading>
-      <div className="container py-14 mx-auto">
+      <div className="container mt-6 mx-auto">
         <div className="flex justify-center flex-wrap -m-4">
           {featuredProducts.map((product) => (
             <ProductCard key={product._id} product={product} />
