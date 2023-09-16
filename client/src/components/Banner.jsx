74756e13e5ee7ac07d668cec6b8d40e612187df3
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-16 23:50:11 +0545

    Fixed banner stylings

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -14,11 +14,11 @@ function Banner({
     <div className="grid grid-cols-1 md:grid-cols-2 items-center bg-amber-50 rounded-lg overflow-hidden">
       {/* left side  */}
       <div
-        className={`flex gap-8 justify-center items-center flex-col mb-4 p-4 ${
+        className={`flex gap-4 justify-center items-center flex-col mb-4 p-4 ${
           imagePosition === "left" ? "md:order-last" : ""
         }`}
       >
-        <h1 className="text-4xl lg:text-5xl font-bold uppercase tracking-tighter text-gray-800">
+        <h1 className="text-2xl lg:text-3xl font-bold uppercase tracking-tighter text-gray-800">
           {heading}
         </h1>
         <p className="text-sm text-center tracking-wide">
@@ -26,7 +26,7 @@ function Banner({
           <span className="block">{spanParagraph}</span>
         </p>
         <button
-          className="border-2 border-black py-1 md:py-2 px-6 tracking-wide font-bold uppercase hover:-translate-y-1 transition-transform ease-out duration-100"
+          className="border-2 border-black text-sm py-1 md:py-2 px-6 tracking-wide font-bold uppercase hover:-translate-y-1 transition-transform ease-out duration-100"
           onClick={() => router.push(link)}
         >
           Explore
