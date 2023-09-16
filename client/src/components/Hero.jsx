7fdd892d3f4d752c5ca8c6f8f4fc1efe3f20d684
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-16 20:34:17 +0545

    Fixed the height of hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,8 +4,8 @@ import Heading from "./Heading";
 
 function Hero() {
   return (
-    <section className="text-gray-600 flex justify-center items-center">
-      <div className="container mx-auto flex md:flex-row md:py-24 flex-col items-center">
+    <section className="text-gray-600 flex justify-center items-center h-[50dvh]">
+      <div className="container mx-auto flex md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <Heading>Elevate Your Style with Posaak</Heading>
           <p className="mb-8 leading-relaxed">
